97e0fd72a0dd64522e8a251a43c635b0c5931650
csm <contatocsmpm@gmail.com>
2024-09-06 11:49:42 -0300

    update-dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevelopersModule } from './developers/developers.module';
 
+require('dotenv').config()
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'postgres',
-      password: ,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'nestjs_backend',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
